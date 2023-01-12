387868ae38eb824f5648148fcdb782d343aaa869
ankonahouston <ankona.banerjee@bcm.edu>
2023-01-11 19:09:55 -0600

    Changes made by Ankona for renaming

--- tests/testthat.R
+++ tests/testthat.R
@@ -7,6 +7,6 @@
 # * https://testthat.r-lib.org/reference/test_package.html#special-files
 
 library(testthat)
-library(CanadianOralOpioids)
+library(OralOpioids)
 
-test_check("CanadianOralOpioids")
+test_check("OralOpioids")
